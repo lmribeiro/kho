324dfd74064ed9d3fac681139771fd40ae0b3c50
Luís Ribeiro <luis.mribeiro@sapo.pt>
2020-08-30 23:42:55 +0100

    Delete unused files

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -6,32 +6,38 @@ use Yii;
 use yii\base\Model;
 
 /**
- * LoginForm is the model behind the login form.
+ * Login form
  *
- * @property User|null $user This property is read-only.
- *
+ * @property \app\models\User|null $user
  */
 class LoginForm extends Model
 {
+
     public $username;
     public $password;
-    public $rememberMe = true;
-
+    public $rememberMe;
     private $_user = false;
 
-
     /**
-     * @return array the validation rules.
+     * @inheritdoc
      */
     public function rules()
     {
         return [
             // username and password are both required
             [['username', 'password'], 'required'],
-            // rememberMe must be a boolean value
-            ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
             ['password', 'validatePassword'],
+            ['rememberMe', 'boolean'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'username' => Yii::t('app', 'Username ou Email'),
+            'password' => Yii::t('app', 'Password'),
+            'rememberMe' => Yii::t('app', 'Lembrar-me'),
         ];
     }
 
@@ -46,22 +52,23 @@ class LoginForm extends Model
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
-
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+                $this->addError($attribute, Yii::t('app', 'Username ou password errados.'));
             }
         }
     }
 
     /**
      * Logs in a user using the provided username and password.
-     * @return bool whether the user is logged in successfully
+     *
+     * @return boolean whether the user is logged in successfully
      */
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
+
         return false;
     }
 
@@ -72,10 +79,15 @@ class LoginForm extends Model
      */
     public function getUser()
     {
-        if ($this->_user === false) {
+        if (!$this->_user) {
             $this->_user = User::findByUsername($this->username);
+        }
+
+        if (!$this->_user) {
+            $this->_user = User::findByEmail($this->username);
         }
 
         return $this->_user;
     }
+
 }
---
+++ views/layouts/login.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @var $this \yii\web\View */
+
+/* @var $content string */
+
+use app\assets\AppAsset;
+use yii\helpers\Html;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="container-fluid bg-soft">
+    <?php include __DIR__ . '/_nav.php'; ?>
+    <div class="row">
+        <div class="col-12">
+            <?php include __DIR__ . '/_sidebar.php'; ?>
+            <main class="content">
+                <?php include __DIR__ . '/_topbar.php'; ?>
+                <?= $content ?>
+            </main>
+        </div>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
